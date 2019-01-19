076789b45f54d1304e332eec0ee4d2a094a89dca
Mimerel <support@mimerel.fr>
2019-01-19 14:16:43 +0100

    Added All to retrieve all data

--- main.go
+++ main.go
@@ -14,14 +14,11 @@ import (
 	"strings"
 )
 
-
-
 var log = logging.MustGetLogger("default")
 
 var format = logging.MustStringFormatter(
 	`%{color}%{time:15:04:05.000} %{shortfunc} ▶ %{level:.4s} %{color:reset} %{message}`,
 )
-
 
 func main() {
 	backend := logging.NewLogBackend(os.Stderr, "", 0)
@@ -32,16 +29,16 @@ func main() {
 
 	config := _package.ReadConfiguration()
 	Port := config.Port
-	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		urlPath := r.URL.Path
 		urlParams := strings.Split(urlPath, "/")
-		if len(urlParams) > 1  {
+		if len(urlParams) > 1 {
 			collectMetricValuesAndSend(w, r, urlParams, &config)
 		} else {
 			w.WriteHeader(500)
 		}
 	})
-	http.ListenAndServe(":" + Port, nil)
+	http.ListenAndServe(":"+Port, nil)
 }
 
 func collectMetricValuesAndSend(w http.ResponseWriter, r *http.Request, urlParams []string, config *_package.Configuration) {
@@ -59,20 +56,29 @@ func collectMetricValuesAndSend(w http.ResponseWriter, r *http.Request, urlParam
 		logs.Error(config.Elasticsearch.Url, config.Host, fmt.Sprintf("Unable to yaml to structure %s ", err))
 
 	}
-	for key, value := range urlParams {
-		if key != 0 {
-			result, err := requestMetric(data, value)
-			if err != nil {
-				logs.Error(config.Elasticsearch.Url, config.Host, fmt.Sprintf("Unable to find value for metric %s %s", value, err))
-			} else {
-				results = append(results, result)
+	var js []byte
+	if urlParams[1] != "all" {
+		for key, value := range urlParams {
+			if key != 0 {
+				result, err := requestMetric(data, value)
+				if err != nil {
+					logs.Error(config.Elasticsearch.Url, config.Host, fmt.Sprintf("Unable to find value for metric %s %s", value, err))
+				} else {
+					results = append(results, result)
+				}
 			}
 		}
-	}
-	js, err := json.Marshal(results)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		js, err = json.Marshal(results)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	} else {
+		js, err = json.Marshal(data)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(js)
@@ -81,12 +87,12 @@ func collectMetricValuesAndSend(w http.ResponseWriter, r *http.Request, urlParam
 func requestMetric(data []_package.StructuredData, metricName string) (_package.Result, error) {
 	for _, value := range data {
 		if value.Metric == metricName {
-			metricValue, err := strconv.ParseFloat(value.Value,  64)
+			metricValue, err := strconv.ParseFloat(value.Value, 64)
 			if err != nil {
-				return _package.Result{} , fmt.Errorf("Unable to convert metric value to float")
+				return _package.Result{}, fmt.Errorf("Unable to convert metric value to float")
 			}
 			return _package.Result{metricName, metricValue}, nil
 		}
 	}
-	return _package.Result{} , fmt.Errorf("Unable to find metric in storage_file")
+	return _package.Result{}, fmt.Errorf("Unable to find metric in storage_file")
 }
